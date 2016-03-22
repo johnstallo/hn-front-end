a762e08760c51ac3ffc7ce6f7ad0184e412f0c2f
Giovanni Stallo <johnstallo@giovannis-mbp.guest.corp.microsoft.com>
2016-03-22 14:29:15 -0700

    demo reset

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    remoteUrl : process.env.MONGODB_URL,
-    //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    //remoteUrl : process.env.MONGODB_URL,
+    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
