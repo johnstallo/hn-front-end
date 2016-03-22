cb5ca899a0c87b65cd8b91dbfa1d84ad552ffe25
Giovanni Stallo <johnstallo@giovannis-mbp.guest.corp.microsoft.com>
2016-03-22 13:30:50 -0700

    hosted mongo

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    remoteUrl : process.env.MONGODB_URL,
+    //remoteUrl : process.env.MONGODB_URL,
+    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
