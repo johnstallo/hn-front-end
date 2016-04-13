42c4ea882cd58e55684a810bfc1ed5159ffd22ac
John Stallo <john.stallo@live.com>
2016-04-13 01:33:03 -0700

    simplify UI

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -46,22 +46,6 @@ angular.module('articleController', [])
                 });
         };
 
-        $scope.extractDomain = function(url) {
-            var domain;
-            //find & remove protocol (http, ftp, etc.) and get domain
-            if (url.indexOf("://") > -1) {
-                domain = url.split('/')[2];
-            }
-            else {
-                domain = url.split('/')[0];
-            }
-
-            //find & remove port number
-            domain = domain.split(':')[0];
-
-            return domain;
-        };
-        
         $scope.upvoteArticle = function(articleID) {
           Articles.upvote(articleID)
             .then(function(data){
